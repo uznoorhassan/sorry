1e175a306b3cd60dd83c54446032e7c56779bc34
uznoorhassan <uznoorhassan@gmail.com>
2026-04-15 21:02:02 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,53 @@
+let noClickCount = 0;
+
+function handlenoclick() {
+    const noButton = document.querySelector(".nobutton");
+    const yesButton = document.querySelector(".yesbutton");
+
+    noClickCount++;
+
+    const messages = [
+        "Are you sure?",
+        "Really sure?",
+        "Think again 🥺",
+        "Pleaseeee 💔",
+        ":c",
+        "I'm sorry mama",
+        "NOOOOO",
+        "YEAH I KNOW YOU WANT TO CLICK YES",
+        "MAMA PLEAASSSEE",
+    ];
+
+    // Change NO button text
+    if (noClickCount <= messages.length) {
+        noButton.textContent = messages[noClickCount - 1];
+    }
+
+    // Grow YES button
+    let currentSize = 1 + (noClickCount * 0.5);
+    yesButton.style.transform = `scale(${currentSize})`;
+
+    // Move NO button randomly
+    moveButtonRandomly(noButton);
+}
+
+function moveButtonRandomly(button) {
+    const container = document.querySelector(".container");
+
+    const containerRect = container.getBoundingClientRect();
+    const buttonRect = button.getBoundingClientRect();
+
+    const maxX = containerRect.width - buttonRect.width;
+    const maxY = containerRect.height - buttonRect.height;
+
+    const randomX = Math.random() * maxX;
+    const randomY = Math.random() * maxY;
+
+    button.style.position = "absolute";
+    button.style.left = randomX + "px";
+    button.style.top = randomY + "px";
+}
+
+function handleyesclick() {
+    window.location.href = "yes.html";
+}
